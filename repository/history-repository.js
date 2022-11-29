313fb093bc690b958440022c6e0513f6c44878ae
Pablo Silva <pablo.juninho2018@gmail.com>
2022-11-28 22:57:58 -0300

    finalizada implementação do histórico

--- repository/history-repository.js
+++ repository/history-repository.js
@@ -3,12 +3,47 @@ const pool = require("./dbconfig.js").pool;
 module.exports = {
     findAllHistory: async function () {
         return pool.query(
-            "SELECT * FROM movimento_peca ORDER BY horario DESC"
+           `select 
+                movimento_peca.id,
+                perfil_peca.descricao as peca,
+                perfil_cliente.nome as cliente,
+                usuario.nome as vendedor,
+                perfil_peca.valor,
+                movimento_peca.horario
+            from
+                movimento_peca,
+                perfil_peca,
+                perfil_cliente,
+                usuario
+            where
+                movimento_peca.id_peca = perfil_peca.id and
+                movimento_peca.id_vendedor = usuario.id and
+                movimento_peca.id_cliente = perfil_cliente.id
+            order by
+                movimento_peca.horario desc;`
         );
     },
     findAllHistoryById: async function (clientid) {
         return pool.query(
-            "SELECT * FROM movimento_peca WHERE movimento_peca.id_cliente = ? ORDER BY horario DESC", clientid
+            `select 
+                movimento_peca.id,
+                perfil_peca.descricao as peca,
+                perfil_cliente.nome as cliente,
+                usuario.nome as vendedor,
+                perfil_peca.valor,
+                movimento_peca.horario
+            from
+                movimento_peca,
+                perfil_peca,
+                perfil_cliente,
+                usuario
+            where
+                movimento_peca.id_peca = perfil_peca.id and
+                movimento_peca.id_vendedor = usuario.id and
+                movimento_peca.id_cliente = perfil_cliente.id and
+                perfil_cliente.id = ?
+            order by
+                movimento_peca.horario desc;`, clientid
         );
     }
 }
